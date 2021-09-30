d6dd636179771b616db0f411b921b70ea3d7b107
vlad.shurin <vlad.shurin1997@gmail.com>
2021-09-30 19:16:10 +0300

    create project

---
+++ TyreService/TyreServiceTests/TyreServiceTests.swift
@@ -0,0 +1,33 @@
+//
+//  TyreServiceTests.swift
+//  TyreServiceTests
+//
+//  Created by ULADZISLAU SHURYN on 30.09.21.
+//
+
+import XCTest
+@testable import TyreService
+
+class TyreServiceTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
